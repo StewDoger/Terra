8a19fff34591c5ce95564eea6555a7191df7116f
brodan999 <mohamadardan1928@gmail.com>
2023-05-06 04:24:40 +0700

    delete text in explore

--- lib/screens/Explore/explore_page.dart
+++ lib/screens/Explore/explore_page.dart
@@ -9,11 +9,6 @@ import 'package:terra_apps/screens/Posts/postView_model.dart';
 final _store = FirebaseFirestore.instance;
 
 class Explore extends StatelessWidget {
-//   @override
-//   _ExploreState createState() => _ExploreState();
-// }
-
-// class _ExploreState extends State<Explore> {
 
   List<Color> _colors = [
     Colors.red,
